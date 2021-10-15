098551ae582fe2979bbf495d2391a79bb71c7722
Cristian Hernandez <christianhb534@gmail.com>
2021-10-15 03:42:58 -0500

    httpclient

--- src/controllers/httpClient.js
+++ src/controllers/httpClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
+    baseURL: "http://localhost:3000",
     timeout: 3000,
     headers: {
         "Content-Type": "application/json"
